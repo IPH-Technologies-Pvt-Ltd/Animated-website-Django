4ebbc0c529eae7bf63b3945a319fcf33a086281a
developerMaruya <errbmaurya@gmail.com>
2023-07-03 18:24:52 +0530

    3d-webDesign

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rbdemo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ThreeDwebsite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
